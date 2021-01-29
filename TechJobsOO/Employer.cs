430563d1b6b347a350f8f3ab75eb2efbdb5f8001
Ethan Kovalenko <ashervoronin@gmail.com>
2021-01-29 12:27:43 -0600

    Looked over the Employer class, and then added the second constructor to the best of my knowledge in the Location class.

--- TechJobsOO/Employer.cs
+++ TechJobsOO/Employer.cs
@@ -4,15 +4,21 @@ namespace TechJobsOO
     public class Employer
     {
         public int Id { get; }
+        // Declares nextId as a static field that is not stored within any Employer object
         private static int nextId = 1;
         public string Value { get; set; }
 
+        // Accepts no arguments and assigns the value of nextId to the Id field. It then increments nextId.
+        // Thus, every new Employer object will get a different Id number
         public Employer()
         {
             Id = nextId;
             nextId++;
         }
 
+        // Assigns the value field. It ALSO initiliazes Id for the object by calling the first constructor
+        // statement with the : this() syntax. Including : this() in any Employer constructor makes initializing
+        // Id a default behavior. This is known as constructor chaining.
         public Employer(string value) : this()
         {
             Value = value;
